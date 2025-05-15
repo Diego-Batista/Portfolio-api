13c8541adf725db4f078fbbfb9e4fb3c1bca01e8
Diego-Batista <diegobtistadev@gmail.com>
2025-05-15 14:39:26 -0300

    chore:  initial structure and add eslint and prettier setup

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+
+const server = express();
+server.use(cors());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true}));
+server.use(express.static('public'));
+
+server.get('/ping', (req, res) => {
+    res.json({ pong: true})
+});
+
+server.listen(4444, () => {
+    console.log('Portfolio Diego backend running...')
+});
